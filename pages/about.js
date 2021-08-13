7e32af51c0b54030cfe26c848ee2692353779660
prasanthbattula <prasanthbattula5@gmail.com>
2021-08-13 12:38:35 +0530

    sub page test added

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Sub Page</title>
+        <meta name="description" content="Sub page sharing" />
+      </Head>
+      <h1>Sub Page</h1>
+    </div>
+  );
+}
